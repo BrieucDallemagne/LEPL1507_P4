38d17596563f6870fd064943989f7e03c56c7271
alexxtrem <127242251+alexxtrem@users.noreply.github.com>
2024-02-10 11:11:16 +0100

    fonction solve_2D qui permet de trouver le nbre de satellites optimal (et leurs positions)

--- fonction_math.py
+++ fonction_math.py
@@ -40,7 +40,7 @@ def city_intensities(cities_coords, satellites_coords, satellite_power):
         intensities[i] = total_intensity(city_coord, satellites_coords, satellite_power)
     return intensities
 
-def is_covered(city_coords, satellites_coords):
+def is_covered_3D(city_coords, satellites_coords):
     """
     Check whether or not the city is covered by at least one satellite
     """
@@ -53,14 +53,14 @@ def is_covered(city_coords, satellites_coords):
             return True
     return False
 
-def plot_covering(cities_coordinates, satellites_coordinates):
+def plot_covering_3D(cities_coordinates, satellites_coordinates):
     
     # Extraire les coordonnées x et y des points
     x_coords, y_coords, z_coords = zip(*cities_coordinates)
 
     # Tracer les points
     for x_coord, y_coord in zip(x_coords, y_coords):
-        plt.scatter(x_coord, y_coord, color='green' if is_covered(np.array([x_coord, y_coord]), satellites_coordinates) else 'red', marker='o')
+        plt.scatter(x_coord, y_coord, color='green' if is_covered_3D(np.array([x_coord, y_coord]), satellites_coordinates) else 'red', marker='o')
 
     # Tracer les cercles
     for center, radius in zip(satellites_coordinates[:, :2], np.sqrt(satellites_coordinates[:, 3]**2 - satellites_coordinates[:, 2]**2)):
@@ -74,33 +74,125 @@ def plot_covering(cities_coordinates, satellites_coordinates):
     
     plt.show()
 
+def is_covered_2D(city_coords, satellites_coords):
+    """
+    Check whether or not the city is covered by at least one satellite
+    """
+    for satellite_coords in satellites_coords:
+        city_satellite_distance = math.sqrt((city_coords[0]-satellite_coords[0])**2 + (city_coords[1]-satellite_coords[1])**2)
+        if satellite_coords[2] >= city_satellite_distance:
+            return True
+    return False
+
+def plot_covering_2D(cities_coordinates, satellites_coordinates, grid_size):
+    
+    # Extraire les coordonnées x et y des points
+    x_coords, y_coords = zip(*cities_coordinates)
+
+    # Tracer les points
+    for x_coord, y_coord in zip(x_coords, y_coords):
+        plt.scatter(x_coord, y_coord, color='green' if is_covered_2D(np.array([x_coord, y_coord]), satellites_coordinates) else 'red', marker='o')
+
+    # Tracer les cercles
+    for center, radius in zip(satellites_coordinates[:, :2], satellites_coordinates[:, 2]):
+        circle = Circle(center, radius, edgecolor='blue', facecolor='none')
+        plt.gca().add_patch(circle)
+    
+    plt.scatter(satellites_coordinates[:, 0], satellites_coordinates[:, 1], color='blue', marker='x', label='Satellites')
+    
+    for x in range(grid_size+1):
+        for y in range(grid_size+1):
+            plt.plot(x, y, 'ko', markersize=0.5)
+
+    plt.title('Network coverage')
+    plt.xlabel('X coordinate')
+    plt.ylabel('Y coordinate')
+    plt.axis('equal')
+    plt.xlim(0, grid_size+1)
+    plt.ylim(0, grid_size+1)
+    
+    plt.show()
 
 
-"""Quelques exemples"""
-city_A = np.array([1, 2, 0]) # x, y, z
+
+
+"""city_A = np.array([1, 2, 0]) # x, y, z
 city_B = np.array([4, 8, 0])
 city_C = np.array([5, 2, 0])
 city_D = np.array([8, 14, 0])
 city_E = np.array([10, 6, 0])
-satellite_A = np.array([2, 5, 12, 13]) # x, y, z, radius
+satellite_A = np.array([2, 5, 12, 13]) # x, y, z, range
 satellite_B = np.array([4, 3, 12, 13])
 satellite_C = np.array([11, 8, 12, 12.5])
 cities_coordinates = np.array([city_A, city_B, city_C, city_D, city_E])
-satellites_coordinates = np.array([satellite_A, satellite_B, satellite_C])
+satellites_coordinates = np.array([satellite_A, satellite_B, satellite_C])"""
 
-"""print(euclildean_distance(city_A, satellite_A[:3]))
-print(euclildean_distance(city_A, satellite_B[:3]))
-print(euclildean_distance(city_A, satellite_C[:3]))
 
-print(total_intensity(city_A, satellites_coordinates[:, :3], 100))
 
-print(city_intensities(cities_coordinates, satellites_coordinates[:, :3], 100))
-"""
+import cvxpy as cp
 
-print(is_covered(city_A, satellites_coordinates))
-print(is_covered(city_B, satellites_coordinates))
-print(is_covered(city_C, satellites_coordinates))
-print(is_covered(city_D, satellites_coordinates))
-print(is_covered(city_E, satellites_coordinates))
+def solve_2D(cities_coordinates, grid_size, radius):
 
-plot_covering(cities_coordinates, satellites_coordinates)
+    num_cities = cities_coordinates.shape[0]
+
+    # Create a matrix for distances
+    distances_matrix = np.zeros((num_cities, (grid_size + 1)**2))
+
+    for i in range(num_cities):
+        city_x, city_y = cities_coordinates[i]
+        for j in range((grid_size + 1)**2): # (0,0) puis (0,1) puis (0,2) ...
+            x_pos = j // (grid_size + 1)
+            y_pos = j % (grid_size + 1)
+            distances_matrix[i, j] = np.linalg.norm([city_x - x_pos, city_y - y_pos])
+
+    # Variables
+    satellite_positions = cp.Variable((grid_size + 1)**2, boolean=True)
+
+    # Objective
+    objective = cp.Minimize(cp.sum(satellite_positions))
+
+    # Constraints
+    constraints = []
+    for i in range(num_cities):
+        indices = []
+        for j in range((grid_size + 1)**2):
+            if distances_matrix[i, j] <= radius:
+                indices.append(j)
+
+        city_coverage = cp.sum(satellite_positions[indices])
+        constraints.append(city_coverage >= 1)
+
+    # Solve
+    problem = cp.Problem(objective, constraints)
+    problem.solve(solver=cp.GLPK_MI)
+    solution_matrix = satellite_positions.value.astype(int).reshape(grid_size+1,grid_size+1)
+
+    # Results
+    print("Optimal satellite positions:")
+    for city_coordinates in cities_coordinates:
+        solution_matrix[city_coordinates[0],city_coordinates[1]] = 2
+    print(solution_matrix)
+    print("Optimal number of satellites: {}".format(problem.value))
+
+    satellites_coordinates = np.array(np.hstack((np.argwhere(solution_matrix == 1), np.full((int(problem.value), 1), radius))))
+
+    return satellites_coordinates
+
+
+
+cities_coordinates = np.array([
+    [1, 2],
+    [4, 8],
+    [5, 2],
+    [8, 14],
+    [10, 6],
+    [15, 3],
+    [15, 15],
+    [16, 16],
+    [24, 21],
+    #[25,12]
+])
+
+satellites_coordinates = solve_2D(cities_coordinates, 25, 8)
+
+plot_covering_2D(cities_coordinates, satellites_coordinates, 25)

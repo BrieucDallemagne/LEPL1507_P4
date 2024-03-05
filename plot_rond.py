7fd28ddd647a892457e4534519cb02a54e3fd598
Pierre <pierre.leboutte@student.uclouvain.be>
2024-03-05 11:42:26 +0100

    Scusez normalement c bon?

--- plot_rond.py
+++ plot_rond.py
@@ -1,15 +1,18 @@
+import spherical_satellites_repartition as ssr
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import fonction_math as fm
 import math
-from matplotlib import animation
+
 
 def plot_3D_old(satellites_coordinates, cities_coordinates, kmeans=False, original_cities_coordinates=np.array(0)):
     fig = plt.figure(figsize=(10, 8))
     ax = fig.add_subplot(111, projection='3d')
 
-    radius_earth = 6371  
-    latitudes = np.linspace(-90, 90, 100)  
-    longitudes = np.linspace(-180, 180, 200)  
+    radius_earth = 6371
+    latitudes = np.linspace(-90, 90, 100)
+    longitudes = np.linspace(-180, 180, 200)
     longitude_grid, latitude_grid = np.meshgrid(np.radians(longitudes), np.radians(latitudes))
     x = radius_earth * np.cos(latitude_grid) * np.cos(longitude_grid)
     y = radius_earth * np.cos(latitude_grid) * np.sin(longitude_grid)
@@ -23,13 +26,17 @@ def plot_3D_old(satellites_coordinates, cities_coordinates, kmeans=False, origin
     ax.scatter(satellites_x, satellites_y, satellites_z, color='r', marker='o', label='Satellites')
 
     # Plot cities
-    cities_x = [radius_earth * np.cos(np.radians(coord[1])) * np.cos(np.radians(coord[0])) for coord in cities_coordinates]
-    cities_y = [radius_earth * np.cos(np.radians(coord[1])) * np.sin(np.radians(coord[0])) for coord in cities_coordinates]
+    cities_x = [radius_earth * np.cos(np.radians(coord[1])) * np.cos(np.radians(coord[0])) for coord in
+                cities_coordinates]
+    cities_y = [radius_earth * np.cos(np.radians(coord[1])) * np.sin(np.radians(coord[0])) for coord in
+                cities_coordinates]
     cities_z = [radius_earth * np.sin(np.radians(coord[1])) for coord in cities_coordinates]
     if kmeans:
         ax.scatter(cities_x, cities_y, cities_z, color='g', marker='^', label='Centroïds')
-        original_x = [radius_earth * np.cos(np.radians(coord[1])) * np.cos(np.radians(coord[0])) for coord in cities_coordinates]
-        original_y = [radius_earth * np.cos(np.radians(coord[1])) * np.sin(np.radians(coord[0])) for coord in cities_coordinates]
+        original_x = [radius_earth * np.cos(np.radians(coord[1])) * np.cos(np.radians(coord[0])) for coord in
+                      cities_coordinates]
+        original_y = [radius_earth * np.cos(np.radians(coord[1])) * np.sin(np.radians(coord[0])) for coord in
+                      cities_coordinates]
         original_z = [radius_earth * np.sin(np.radians(coord[1])) for coord in original_cities_coordinates]
         ax.scatter(original_x, original_y, original_z, color='y', marker='^', label='Cities')
     else:
@@ -44,19 +51,21 @@ def plot_3D_old(satellites_coordinates, cities_coordinates, kmeans=False, origin
 
     plt.show()
 
+
 def is_covered_3D(city_coords, satellites_coords, scope):
     """
     Check whether or not the city is covered by at least one satellite
     """
     for satellite_coords in satellites_coords:
-        city_satellite_distance = math.sqrt((city_coords[0]-satellite_coords[0])**2 + (city_coords[1]-satellite_coords[1])**2 + (city_coords[2]-satellite_coords[2])**2)
+        city_satellite_distance = math.sqrt(
+            (city_coords[0] - satellite_coords[0]) ** 2 + (city_coords[1] - satellite_coords[1]) ** 2 + (
+                        city_coords[2] - satellite_coords[2]) ** 2)
         if scope >= city_satellite_distance:
             return True
     return False
 
 
-def plot_3D(cities_coordinates, satellites_coordinates, cities_weights, scope, height, kmeans=False, original_cities_coordinates=np.array(0), original_cities_weights=np.array(0), rot=False):
-
+def plot_3D(cities_coordinates, satellites_coordinates, height):
     sphere_center = (0, 0, 0)
 
     # Créer le plot en 3D
@@ -66,6 +75,7 @@ def plot_3D(cities_coordinates, satellites_coordinates, cities_weights, scope, h
     # Rayon de la sphère
     earth_radius = 50
     satellite_radius = 50 + height
+    scope = fm.find_x(height, earth_radius)
 
     # Créer la terre
     u = np.linspace(0, 2 * np.pi, 25)
@@ -86,20 +96,22 @@ def plot_3D(cities_coordinates, satellites_coordinates, cities_weights, scope, h
     y_grid = sphere_center[1] + satellite_radius * np.sin(phi) * np.sin(theta)
     z_grid = sphere_center[2] + satellite_radius * np.cos(phi)
 
-    #ax.plot_wireframe(x_grid, y_grid, z_grid, color='black', linewidth=0.5)
+    # ax.plot_wireframe(x_grid, y_grid, z_grid, color='black', linewidth=0.5)
 
-    #ax.scatter(x_grid, y_grid, z_grid, color='red', s=10, alpha=0.2)
+    # ax.scatter(x_grid, y_grid, z_grid, color='red', s=10, alpha=0.2)
 
-    x_sat = sphere_center[0] + satellite_radius * np.sin(satellites_coordinates[:, 1]) * np.cos(satellites_coordinates[:, 0])
-    y_sat = sphere_center[1] + satellite_radius * np.sin(satellites_coordinates[:, 1]) * np.sin(satellites_coordinates[:, 0])
+    x_sat = sphere_center[0] + satellite_radius * np.sin(satellites_coordinates[:, 1]) * np.cos(
+        satellites_coordinates[:, 0])
+    y_sat = sphere_center[1] + satellite_radius * np.sin(satellites_coordinates[:, 1]) * np.sin(
+        satellites_coordinates[:, 0])
     z_sat = sphere_center[2] + satellite_radius * np.cos(satellites_coordinates[:, 1])
 
-    #print(satellites_coordinates)
-    #print(x_sat)
-    #print(y_sat)
-    #print(z_sat)
+    # print(satellites_coordinates)
+    # print(x_sat)
+    # print(y_sat)
+    # print(z_sat)
     satellites_spherical_coordinates = np.c_[x_sat, y_sat, z_sat]
-    #print(satellites_spherical_coordinates)
+    # print(satellites_spherical_coordinates)
 
     ax.scatter(x_sat, y_sat, z_sat, color='blue', s=40, label='Satellites')
     for x_s, y_s, z_s in zip(x_sat, y_sat, z_sat):
@@ -108,28 +120,18 @@ def plot_3D(cities_coordinates, satellites_coordinates, cities_weights, scope, h
         z = z_s + scope * np.outer(np.ones(np.size(u)), np.cos(v))
         ax.plot_surface(x, y, z, color='gray', alpha=0.3)
 
-
     for x_city, y_city, z_city in cities_coordinates:
         is_covered = is_covered_3D([x_city, y_city, z_city], satellites_spherical_coordinates, scope)
-        #print("City ({}, {}, {}) is covered: {}".format(x_city, y_city, z_city, is_covered))
+        # print("City ({}, {}, {}) is covered: {}".format(x_city, y_city, z_city, is_covered))
         ax.scatter(x_city, y_city, z_city, c='green' if is_covered else "red", s=20, marker='o')
-    i= 0
-    if kmeans:
-        for x_city, y_city, z_city in original_cities_coordinates:
-            is_covered = is_covered_3D([x_city, y_city, z_city], satellites_spherical_coordinates, scope)
-            # print("City ({}, {}, {}) is covered: {}".format(x_city, y_city, z_city, is_covered))
-            ax.scatter(x_city, y_city, z_city, c='pink' if is_covered else "orange", s=20, marker='o')
-            ax.text(x_city, y_city, z_city, '%s' % (str(original_cities_weights[i])), size=5, zorder=1,
-                    color='k')
-            i+=1
-            # Configurer les limites de l'axe
+
+    # Configurer les limites de l'axe
     ax.set_xlim(-70, 70)
     ax.set_ylim(-70, 70)
     ax.set_zlim(-70, 70)
 
     # Make all axes equal in size
     ax.set_box_aspect([1, 1, 1])
-
 
     # Ajouter une légende
     ax.legend()
@@ -138,11 +140,4 @@ def plot_3D(cities_coordinates, satellites_coordinates, cities_weights, scope, h
     ax.set_xlabel('X')
     ax.set_ylabel('Y')
     ax.set_zlabel('Z')
-    if rot:
-        def animate(frame):
-            ax.view_init(30, 6*frame)
-            plt.pause(.001)
-            return fig
-
-        anim = animation.FuncAnimation(fig, animate, frames=200, interval=100)
     plt.show()
--- test_rond.py
+++ test_rond.py
@@ -1,45 +1,58 @@
+import spherical_satellites_repartition as ssr
 import numpy as np
 import matplotlib.pyplot as plt
+import random
+from mpl_toolkits.mplot3d import Axes3D
 import fonction_math as fm
-import spherical_satellites_repartition as ssr
 import plot_rond as pr
+import math
 import matplotlib
-matplotlib.use('TkAgg')
-def test_solve_3D_random(n_tests=5, k_means=False, fix_seed = False):
-    for i in range(n_tests):
-        if fix_seed:
-            n_cities = 5
-            cities_weights = [0.2, 0.2, 0.2, 0.2, 0.2]
-            radius_earth = 50
-            cities_coordinates_latitude = [0, 45, -45, 0, 0]
-            cities_coordinates_longitude = [0, 30, 60, 90, 120]
-        else:
-            n_cities = np.random.randint(2, 20)
-            #cities_weights = fm.create_weight(n_cities)
-            cities_weights = np.full(n_cities, 1/n_cities)
-            radius_earth = 50
 
-            cities_coordinates_latitude = np.random.randint(-90, 90, size=(n_cities))
-            cities_coordinates_longitude = np.random.randint(-180, 180, size=(n_cities))
+matplotlib.use('TkAgg')
+
+
+def test_solve_3D_random(k_means=False):
+    n_cities = 5
+    cities_weights = fm.create_weight(n_cities)
+
+
+def test_solve_3D_random(n_tests=5, k_means=False):
+    for i in range(n_tests):
+        n_cities = np.random.randint(2, 20)
+        # weights = fm.create_weight(n_cities)
+        weights = np.full(n_cities, 1 / n_cities)
+        new_count = 0
+        cities_weights = np.array([])
+        for weight in range(n_cities):
+            if weights[weight] > 1 / (n_cities * 100):
+                cities_weights = np.append(cities_weights, weights[weight])
+                new_count += 1
+
+        n_cities = new_count
+        radius_earth = 50
+        cities_coordinates_latitude = np.random.randint(-90, 90, size=(n_cities))
+        cities_coordinates_longitude = np.random.randint(-180, 180, size=(n_cities))
         cities_coordinates = np.c_[cities_coordinates_latitude, cities_coordinates_longitude]
 
-        cities_x = [radius_earth * np.cos(np.radians(coord[1])) * np.cos(np.radians(coord[0])) for coord in cities_coordinates]
-        cities_y = [radius_earth * np.cos(np.radians(coord[1])) * np.sin(np.radians(coord[0])) for coord in cities_coordinates]
+        cities_x = [radius_earth * np.cos(np.radians(coord[1])) * np.cos(np.radians(coord[0])) for coord in
+                    cities_coordinates]
+        cities_y = [radius_earth * np.cos(np.radians(coord[1])) * np.sin(np.radians(coord[0])) for coord in
+                    cities_coordinates]
         cities_z = [radius_earth * np.sin(np.radians(coord[1])) for coord in cities_coordinates]
         cities_coordinates = np.c_[cities_x, cities_y, cities_z]
         original_cities = cities_coordinates
-        original_weights = cities_weights
         if k_means:
-            cities_coordinates, cities_weights = fm.k_means_cities(cities_coordinates, n_cities//2, cities_weights)
-        #print(cities_coordinates)
+            cities_coordinates, poids = fm.k_means_cities(cities_coordinates, n_cities - 1, poids)
+        # print(cities_coordinates)
         number_of_satellites = np.random.randint(1, n_cities)
-        satellites_coordinates = ssr.spherical_satellites_repartition(cities_coordinates, cities_weights, 30, 10)
+        satellites_coordinates = ssr.spherical_satellites_repartition(cities_coordinates, cities_weights, 10)
         if np.array_equal(satellites_coordinates, np.array([])):
             continue
         if k_means:
-            pr.plot_3D(cities_coordinates, satellites_coordinates, cities_weights, 30, 10, True, original_cities, original_weights, rot=False )
+            pr.plot_3D(cities_coordinates, satellites_coordinates, 10, True, original_cities)
         else:
-            pr.plot_3D(cities_coordinates, satellites_coordinates, cities_weights, 30, 10, True, original_cities, original_weights)
+            pr.plot_3D(cities_coordinates, satellites_coordinates, 10)
         plt.show()
 
-test_solve_3D_random( n_tests=1, k_means=True, fix_seed = True)
+
+test_solve_3D_random(n_tests=5, k_means=False)
